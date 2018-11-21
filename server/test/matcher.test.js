e53f4119b96eec9088e6762df3ceac2fd39cf207
Matt Searle <msearle@scottlogic.co.uk>
2018-11-21 18:22:48 +0000

    Basic server implementation

--- server/test/matcher.test.js
+++ server/test/matcher.test.js
@@ -1,128 +1,157 @@
 'use strict';
 
-const Matcher = require("../app/matcher.module");
-const OrderModule = require("../app/order.module");
-const TradeModule = require("../app/trade.module");
-
-const Order = OrderModule.Order
-const ActionEnum = OrderModule.ActionEnum
-const Trade = TradeModule.Trade
+const Matcher = require("../app/matcher");
+const { createOrder, createTrade, Action } = require("../app/order");
 
 describe("Matcher", () => {
-    test("Initialise to an empty set or orders", () => {
+    test("Initialise to an empty set of orders", () => {
         expect(new Matcher().trades).toEqual([]);
         expect(new Matcher().unmatchedBuyers).toEqual([]);
         expect(new Matcher().unmatchedSellers).toEqual([]);
     });
 
-    test("Initialise and separate buyer and seller orders", () => {
-        const testBuy = new Order("acc-1", 5.0, 1, ActionEnum.BUY)
-        const testSell = new Order("acc-2", 5.0, 1, ActionEnum.SELL)
+    test("Initialise and separate unmatched buyer and seller orders", () => {
+        const testBuy = createOrder("acc-1", 5.0, 1, Action.BUY)
+        const testSell = createOrder("acc-2", 7.0, 1, Action.SELL)
 
         const matcher = new Matcher([testBuy, testSell]);
         
-        expect(matcher.unmatchedBuyers).toContain(testBuy);
-        expect(matcher.unmatchedSellers).toContain(testSell);
+        expect(matcher.unmatchedBuyers).toEqual([testBuy]);
+        expect(matcher.unmatchedSellers).toEqual([testSell]);
     });
 
-    test("Initialise and sort a set of buyers by price", () => {
-        const orderTime = new Date()
-        const testBuy1 = new Order("acc-1", 5.0, 1, ActionEnum.BUY, orderTime);
-        const testBuy2 = new Order("acc-1", 3.0, 1, ActionEnum.BUY, orderTime);
-        
-        const matcher1 = new Matcher([testBuy1, testBuy2]);
-        const matcher2 = new Matcher([testBuy2, testBuy1]);
+    describe("Initialise and sort", () => {
 
-        expect(matcher1.unmatchedBuyers).toEqual(matcher2.unmatchedBuyers);
-    });
+        test("a set of buyers by price", () => {
+            const orderTime = new Date()
+            const testBuy1 = createOrder("acc-1", 5.0, 1, Action.BUY, orderTime);
+            const testBuy2 = createOrder("acc-1", 3.0, 1, Action.BUY, orderTime);
+            
+            const matcher1 = new Matcher([testBuy1, testBuy2]);
+            const matcher2 = new Matcher([testBuy2, testBuy1]);
+    
+            expect(matcher1.unmatchedBuyers).toEqual(matcher2.unmatchedBuyers);
+        });
+    
+        test("a set of buyers by order time", () => {
+            const testBuy1 = createOrder("acc-1", 5.0, 1, Action.BUY, Date(5000000));
+            const testBuy2 = createOrder("acc-1", 5.0, 1, Action.BUY, Date(3000000));
+            
+            const matcher1 = new Matcher([testBuy1, testBuy2]);
+            const matcher2 = new Matcher([testBuy2, testBuy1]);
+    
+            expect(matcher1.unmatchedBuyers).toEqual(matcher2.unmatchedBuyers);
+        });
+    
+        test("a set of sellers by price", () => {
+            const orderTime = new Date()
+            const testSell1 = createOrder("acc-2", 5.0, 1, Action.SELL, orderTime);
+            const testSell2 = createOrder("acc-2", 3.0, 1, Action.SELL, orderTime);
+            
+            const matcher1 = new Matcher([testSell1, testSell2]);
+            const matcher2 = new Matcher([testSell2, testSell1]);
+    
+            expect(matcher1.unmatchedSellers).toEqual(matcher2.unmatchedSellers);
+        });
+    
+        test("a set of sellers by order time", () => {
+            const testSell1 = createOrder("acc-2", 3.0, 1, Action.SELL, new Date(5000000000));
+            const testSell2 = createOrder("acc-2", 3.0, 1, Action.SELL, new Date(3000000000));
+            
+            console.log(testSell1.orderTime)
+            console.log(testSell2.orderTime)
+    
+            const matcher1 = new Matcher([testSell1, testSell2]);
+            const matcher2 = new Matcher([testSell2, testSell1]);
+    
+            expect(matcher1.unmatchedSellers).toEqual(matcher2.unmatchedSellers);
+        });
 
-    test("Initialise and sort a set of buyers by order time", () => {
-        const testBuy1 = new Order("acc-1", 5.0, 1, ActionEnum.BUY, Date(5000000));
-        const testBuy2 = new Order("acc-1", 5.0, 1, ActionEnum.BUY, Date(3000000));
-        
-        const matcher1 = new Matcher([testBuy1, testBuy2]);
-        const matcher2 = new Matcher([testBuy2, testBuy1]);
+    })
 
-        expect(matcher1.unmatchedBuyers).toEqual(matcher2.unmatchedBuyers);
-    });
-
-    test("Initialise and sort a set of sellers by price", () => {
-        const orderTime = new Date()
-        const testSell1 = new Order("acc-2", 5.0, 1, ActionEnum.SELL, orderTime);
-        const testSell2 = new Order("acc-2", 3.0, 1, ActionEnum.SELL, orderTime);
-        
-        const matcher1 = new Matcher([testSell1, testSell2]);
-        const matcher2 = new Matcher([testSell2, testSell1]);
-
-        expect(matcher1.unmatchedSellers).toEqual(matcher2.unmatchedSellers);
-    });
-
-    test("Initialise and sort a set of sellers by order time", () => {
-        const testSell1 = new Order("acc-2", 3.0, 1, ActionEnum.SELL, new Date(5000000000));
-        const testSell2 = new Order("acc-2", 3.0, 1, ActionEnum.SELL, new Date(3000000000));
-        
-        console.log(testSell1.orderTime)
-        console.log(testSell2.orderTime)
-
-        const matcher1 = new Matcher([testSell1, testSell2]);
-        const matcher2 = new Matcher([testSell2, testSell1]);
-
-        expect(matcher1.unmatchedSellers).toEqual(matcher2.unmatchedSellers);
-    });
-
-    test("receive new buy and sell orders", () => {
-        const testBuy = new Order("acc-1", 5.0, 1, ActionEnum.BUY)
-        const testSell = new Order("acc-2", 5.0, 1, ActionEnum.SELL)
+    test("add new buy and sell orders", () => {
+        const testBuy = createOrder("acc-1", 5.0, 1, Action.BUY)
+        const testSell = createOrder("acc-2", 7.0, 1, Action.SELL)
 
         const matcher = new Matcher();
         matcher.add(testBuy).add(testSell)
 
-        expect(matcher.unmatchedBuyers).toContain(testBuy);
-        expect(matcher.unmatchedSellers).toContain(testSell);
+        expect(matcher.unmatchedBuyers).toEqual([testBuy]);
+        expect(matcher.unmatchedSellers).toEqual([testSell]);
     });
 
     test("match a buyer and a seller if the buyer is willing to pay the asking price", () => {
-        const testBuy = new Order("acc-1", 5.0, 1, ActionEnum.BUY)
-        const testSell = new Order("acc-2", 5.0, 1, ActionEnum.SELL)
+        const testBuy = createOrder("acc-1", 5.0, 1, Action.BUY)
+        const testSell = createOrder("acc-2", 5.0, 1, Action.SELL)
 
         const matcher = new Matcher([testBuy, testSell]);
 
-        expect(matcher.trades).toEqual([new Trade(testBuy, testSell)])
         expect(matcher.unmatchedBuyers).toEqual([])
         expect(matcher.unmatchedSellers).toEqual([])
+        expect(matcher.trades).toEqual([createTrade(testBuy, testSell)])
     });
 
     test("match a buyer and a seller if the buyer is willing to pay more than the asking price", () => {
-        const testBuy = new Order("acc-1", 10.0, 1, ActionEnum.BUY)
-        const testSell = new Order("acc-2", 5.0, 1, ActionEnum.SELL)
+        const testBuy = createOrder("acc-1", 10.0, 1, Action.BUY)
+        const testSell = createOrder("acc-2", 5.0, 1, Action.SELL)
 
         const matcher = new Matcher([testBuy, testSell]);
 
-        expect(matcher.trades).toEqual([new Trade(testBuy, testSell)])
+        expect(matcher.trades).toEqual([createTrade(testBuy, testSell)])
         expect(matcher.unmatchedBuyers).toEqual([])
         expect(matcher.unmatchedSellers).toEqual([])
     });
 
     test("match a buyer to multiple sellers if the buyer wants more than the first seller can provide", () => {
-        const testBuy = new Order("acc-1", 5.0, 2, ActionEnum.BUY)
-        const testSell1 = new Order("acc-2", 5.0, 1, ActionEnum.SELL)
-        const testSell2 = new Order("acc-3", 5.0, 1, ActionEnum.SELL)
+        const testBuy = createOrder("acc-1", 5.0, 2, Action.BUY)
+        const testSell1 = createOrder("acc-2", 5.0, 1, Action.SELL)
+        const testSell2 = createOrder("acc-3", 5.0, 1, Action.SELL)
 
         const matcher = new Matcher([testBuy, testSell1, testSell2]);
 
-        expect(matcher.trades).toEqual([new Trade(testBuy, testSell1), new Trade(testBuy, testSell2)])
+        expect(matcher.trades).toEqual([createTrade(testBuy, testSell1), createTrade(testBuy, testSell2)])
         expect(matcher.unmatchedBuyers).toEqual([])
         expect(matcher.unmatchedSellers).toEqual([])
     });
 
     test("not match a buyer and a seller if the buyer is not willing to pay the asking price", () => {
-        const testBuy = new Order("acc-1", 5.0, 1, ActionEnum.BUY)
-        const testSell = new Order("acc-2", 10.0, 1, ActionEnum.SELL)
+        const testBuy = createOrder("acc-1", 5.0, 1, Action.BUY)
+        const testSell = createOrder("acc-2", 10.0, 1, Action.SELL)
 
         const matcher = new Matcher([testBuy, testSell]);
 
         expect(matcher.trades).toEqual([])
+        expect(matcher.unmatchedBuyers).toEqual([testBuy])
+        expect(matcher.unmatchedSellers).toEqual([testSell])
+    });
+    
+    test("match existing buyers will new sellers once they're added", () => {
+        const testBuy = createOrder("acc-1", 5.0, 1, Action.BUY)
+        const testSell = createOrder("acc-2", 5.0, 1, Action.SELL)
+
+        const matcher = new Matcher([testBuy]);
+        expect(matcher.unmatchedBuyers).toEqual([testBuy])
+        expect(matcher.unmatchedSellers).toEqual([])
+
+        matcher.add(testSell)
+
         expect(matcher.unmatchedBuyers).toEqual([])
         expect(matcher.unmatchedSellers).toEqual([])
+        expect(matcher.trades).toEqual([createTrade(testBuy, testSell)])
+    });
+    
+    test("match existing sellers with new buyers once they're added", () => {
+        const testBuy = createOrder("acc-1", 5.0, 1, Action.BUY)
+        const testSell = createOrder("acc-2", 5.0, 1, Action.SELL)
+
+        const matcher = new Matcher([testSell]);
+        expect(matcher.unmatchedSellers).toEqual([testSell])
+        expect(matcher.unmatchedBuyers).toEqual([])
+
+        matcher.add(testBuy)
+
+        expect(matcher.unmatchedBuyers).toEqual([])
+        expect(matcher.unmatchedSellers).toEqual([])
+        expect(matcher.trades).toEqual([createTrade(testBuy, testSell)])
     });
 });
